28e47fbbce1a8cb6b595dfa9c68c490d05d9d21b
Maadhu <140439336+madegowda11@users.noreply.github.com>
2026-06-05 07:00:26 +0530

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,11 @@
+public class Movie {
+    String title;
+    String genre;
+    double rating;
+
+    public Movie(String title,String genre,double rating){
+        this.title=title;
+        this.genre=genre;
+        this.rating=rating;
+    }
+}
